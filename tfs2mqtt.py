20f2f411294afcfc6c1a2a3278cf56cee820c28e
Scott Ware <scott.r.ware@intel.com>
2022-07-21 14:45:05 +0100

    Add support for MQTT streams
    Signed-off-by: Scott Ware <scott.r.ware@intel.com>

--- tfs2mqtt.py
+++ tfs2mqtt.py
@@ -57,6 +57,7 @@ args = vars(parser.parse_args())
 
 address = "{}:{}".format(args.get('grpc_address'),args.get('grpc_port'))
 
+input = args.get('input')
 model_name = args.get('model_name')
 model_version = args.get('model_version')
 instance_id = args.get('id')
@@ -70,13 +71,50 @@ mqtt_port = args.get('mqtt_port')
 mqtt_username = args.get('mqtt_username')
 mqtt_password = args.get('mqtt_password')
 
-mqtt_topic = ''.join([header, "/", "data", "/", "sensor", "/", category, "/", instance_id])
 command_topic = ''.join([header, "/", "cmd", "/", "sensor", "/", "cam", "/", instance_id])
 image_topic = ''.join([header, "/", "image", "/", "sensor", "/", "cam", "/", instance_id])
 
 # Camera frame & timestamp
 curr_frame = None
 curr_timestamp = None
+mode = None
+
+def process_frame(timestamp, id, height, width, frame):
+    if args.get('perf_stats'):
+        start_time = datetime.now()
+
+    input_name, output_name = get_model_io_names(client, model_name, model_version)
+    inputs = {input_name: frame.tobytes() }
+    results = client.predict(inputs=inputs, model_name=model_name)
+
+    if args.get('perf_stats'):
+        end_time = datetime.now()
+        duration = (end_time - start_time).total_seconds() * 1000
+        print('Processing time: {:.2f} ms; speed {:.2f} fps'.format(round(duration, 2), round(1000 / duration, 2)))
+
+    detections = results[0].reshape(-1, 7)
+
+    objects = []
+
+    for i, detection in enumerate(detections):
+        _, class_id, confidence, x_min, y_min, x_max, y_max = detection
+
+        if confidence > threshold:
+            x_min = int(x_min * width)
+            y_min = int(y_min * height)
+            x_max = int(x_max * width)
+            y_max = int(y_max * height)
+            w = int(x_max - x_min)
+            h = int(y_max - y_min)
+
+            if args.get('debug'):
+                print("detection", i , detection)
+
+            objects.append({"id":i, "category":category, "class":int(class_id), "confidence":float(confidence), "bounding_box":{"x": x_min, "y": y_min, "width": w, "height": h}})
+
+    mqtt_payload = {"timestamp":timestamp,"id":id,"objects":objects}
+    mqtt_topic = ''.join([header, "/", "data", "/", "sensor", "/", category, "/", id])
+    mqttc.publish(mqtt_topic, json.dumps(mqtt_payload))
 
 #
 # Create gRPC client
@@ -102,11 +140,21 @@ def on_connect(mqttc, obj, flags, rc):
     print("MQTT connected...")
 
 def on_message(mqttc, obj, msg):
+    topic = msg.topic
     payload = msg.payload.decode("utf-8")
+
     if payload == 'getimage' and not curr_frame is None:
         jpeg = base64.b64encode(curr_frame).decode('utf-8')
         image_payload = {'timestamp':curr_timestamp.isoformat(timespec='milliseconds').replace("+00:00", "Z"), 'id':instance_id, 'image':jpeg}
         mqttc.publish(image_topic, json.dumps(image_payload))
+
+    else:
+        json_payload = json.loads(payload)
+
+        # Check payload
+        if "timestamp" in json_payload and "id" in json_payload and "height" in json_payload and "width" in json_payload and "frame" in json_payload:
+            img = base64.b64decode(json_payload['frame'])
+            process_frame(json_payload['timestamp'], json_payload['id'], json_payload['height'], json_payload['width'], np.frombuffer(img, dtype=np.uint8))
 
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
@@ -118,81 +166,56 @@ if args.get('mqtt_tls'):
     mqttc.tls_insecure_set(True)
 
 mqttc.connect(mqtt_address, int(mqtt_port), 60)
-mqttc.subscribe(command_topic, 0)
 
 # Limit OpenCV thread pool
 cv2.setNumThreads(args['threads'])
 
-vcap = cv2.VideoCapture()
-status = vcap.open(args['input'])
+# Test for MQTT stream
+if input.startswith('mqtt:'):
+    mqttc.subscribe(input.split(':')[1], 0)
+    mode = 'mqtt'
+else:
+    vcap = cv2.VideoCapture()
+    status = vcap.open(args['input'])
 
-# Test for camera stream
-if not status:
-    print('Failed to open video stream... quitting!')
-    quit()
+    # Test for camera stream
+    if not status:
+        print('Failed to open video stream... quitting!')
+        quit()
+
+    mode = 'video'
+    mqttc.subscribe(command_topic, 0)
 
 print('Start processing frames...')
 
-mqttc.loop_start()
-
-while(1):
-    status, img = vcap.read()
-
-    if not status:
-        # Loop video is applicable
-        if not args.get('loop'):
-            print('No more frames available... Quitting!')
-            quit()
-
-        vcap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+if mode == 'video':
+    mqttc.loop_start()
+    while(1):
         status, img = vcap.read()
 
         if not status:
-            quit()
+            # Loop video is applicable
+            if not args.get('loop'):
+                print('No more frames available... Quitting!')
+                quit()
 
-    # Scale if requested
-    if args.get('width') and args.get('height'):
-        img = cv2.resize(img, (args.get('width'), args.get('height')))
+            vcap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            status, img = vcap.read()
 
-    # Get original image shape
-    ret, curr_frame = cv2.imencode(".jpg", img)
-    orig_height, orig_width, orig_channels = img.shape
+            if not status:
+                quit()
 
-    curr_timestamp = datetime.now(timezone.utc)
+            curr_timestamp = datetime.now(timezone.utc)
+            timestamp_str = curr_timestamp.isoformat(timespec='milliseconds').replace("+00:00", "Z")
 
-    timestamp_str = curr_timestamp.isoformat(timespec='milliseconds').replace("+00:00", "Z")
+            # Scale if requested
+            if args.get('width') and args.get('height'):
+                img = cv2.resize(img, (args.get('width'), args.get('height')))
 
-    if args.get('perf_stats'):
-        start_time = datetime.now()
+            # Get original image shape
+            ret, curr_frame = cv2.imencode(".jpg", img)
+            height, width, channels = img.shape
 
-    input_name, output_name = get_model_io_names(client, model_name, model_version)
-    inputs = {input_name: curr_frame.tobytes() }
-    results = client.predict(inputs=inputs, model_name=model_name)
-
-    if args.get('perf_stats'):
-        end_time = datetime.now()
-        duration = (end_time - start_time).total_seconds() * 1000
-        print('Processing time: {:.2f} ms; speed {:.2f} fps'.format(round(duration, 2), round(1000 / duration, 2)))
-
-    detections = results[0].reshape(-1, 7)
-
-    objects = []
-
-    for i, detection in enumerate(detections):
-        _, class_id, confidence, x_min, y_min, x_max, y_max = detection
-
-        if confidence > threshold:
-            x_min = int(x_min * orig_width)
-            y_min = int(y_min * orig_height)
-            x_max = int(x_max * orig_width)
-            y_max = int(y_max * orig_height)
-            w = int(x_max - x_min)
-            h = int(y_max - y_min)
-
-            if args.get('debug'):
-                print("detection", i , detection)
-
-            objects.append({"id":i, "category":category, "class":int(class_id), "confidence":float(confidence), "bounding_box":{"x": x_min, "y": y_min, "width": w, "height": h}})
-
-    mqtt_payload ={"timestamp":timestamp_str,"id":instance_id,"objects":objects}
-    mqttc.publish(mqtt_topic, json.dumps(mqtt_payload))
+            process_frame(timestamp_str, instance_id, height, width, img)
+else:
+    mqttc.loop_forever()
