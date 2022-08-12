773f7d308497f91010c740c009c138cab22df7be
Scott Ware <scott.r.ware@intel.com>
2022-08-12 22:40:41 +0100

    Add MQTT to benchmark app
    Signed-off-by: Scott Ware <scott.r.ware@intel.com>

--- benchmark.py
+++ benchmark.py
@@ -24,6 +24,8 @@ import simplejpeg
 import ssl
 import threading
 import time
+import uuid
+import paho.mqtt.client as mqtt
 from client_utils import prepare_certs, get_model_io_names, get_model_input_shape
 from datetime import datetime, timezone
 from ovmsclient import make_grpc_client
@@ -32,10 +34,16 @@ from time import sleep
 parser = argparse.ArgumentParser(description='TFS gRPC benchmark utility.')
 
 parser.add_argument('--input', required=True, help='Input image')
+parser.add_argument('--id', required=False, help='Unique identifier for benchmark')
 parser.add_argument('--workers', required=False, help='Workers for frame processing', default=10, type=int)
 parser.add_argument('--scale_input', default=False, action='store_true', help='Scale the input to the correct resolution for the model')
 parser.add_argument('--infer_only', default=False, action='store_true', help='Pre-process input and benchmark inference only')
 parser.add_argument('--binary_input', default=False, action='store_true', help='Use binary inputs')
+parser.add_argument('--mqtt_address',required=False, default='localhost',  help='MQTT broker address. default: localhost')
+parser.add_argument('--mqtt_port',required=False, default=1883, help='MQTT port. default: 1883')
+parser.add_argument('--mqtt_username',required=False, default='',  help='MQTT username.')
+parser.add_argument('--mqtt_password',required=False, default='',  help='MQTT password.')
+parser.add_argument('--mqtt_tls', default=False, action='store_true', help='use TLS communication for MQTT')
 parser.add_argument('--grpc_address',required=False, default='localhost',  help='Specify url to grpc service. default: localhost')
 parser.add_argument('--grpc_port',required=False, default=9000, help='Specify port to grpc service. default: 9000')
 parser.add_argument('--model_name',required=True, help='Specify the model name')
@@ -50,8 +58,13 @@ args = vars(parser.parse_args())
 address = "{}:{}".format(args.get('grpc_address'),args.get('grpc_port'))
 
 image_file = args.get('input')
+instance_id = args.get('id')
 model_name = args.get('model_name')
 model_version = args.get('model_version')
+mqtt_address = args.get('mqtt_address')
+mqtt_port = args.get('mqtt_port')
+mqtt_username = args.get('mqtt_username')
+mqtt_password = args.get('mqtt_password')
 grpc_address = args.get('grpc_address')
 grpc_port = args.get('grpc_port')
 num_workers = args.get('workers')
@@ -63,6 +76,11 @@ perf_stats = args.get('perf_stats')
 # Performance counters
 count = 0
 total_latency = 0
+
+if not instance_id:
+    instance_id = str(uuid.uuid4())
+
+mqtt_topic = "tfs2mqtt/benchmark/" + instance_id
 
 def load_image(path):
     with open(path, 'rb') as f:
@@ -107,9 +125,6 @@ def prepare_input(input_img):
     return {input_name: input_img }
 
 def benchmark_worker():
-    global count
-    global total_latency
-
     # Prepare inputs
     if infer_only:
         inputs = prepare_input(img)
@@ -120,16 +135,19 @@ def benchmark_worker():
         if not infer_only:
             inputs = prepare_input(img)
 
-        results = client.predict(inputs=inputs, model_name=model_name)
+        try:
+            results = client.predict(inputs=inputs, model_name=model_name)
+        except:
+            continue
 
         end_time = time.perf_counter()
         duration = (end_time - start_time)
-        total_latency += duration
+
+        mqttp.publish(mqtt_topic, str(duration), qos=0, retain=False)
 
         if perf_stats:
             print('Processing time:', round(duration, 3))
-            
-        count += 1
+
 
 #
 # Create gRPC client
@@ -146,10 +164,52 @@ if args.get('grpc_tls'):
     }
 
 client = make_grpc_client(address, tls_config=tls_config)
-input_name, output_name = get_model_io_names(client, model_name, model_version)
-input_shape = get_model_input_shape(client, model_name, model_version)
+
+try:
+    input_name, output_name = get_model_io_names(client, model_name, model_version)
+    input_shape = get_model_input_shape(client, model_name, model_version)
+except Exception as error:
+    print(error)
+    quit()
+
 model_height = input_shape[1]
 model_width = input_shape[2]
+
+#
+# Create MQTT clients
+#
+
+def on_connect(mqttc, obj, flags, rc):
+    print("MQTT connected...")
+    mqttc.subscribe(mqtt_topic, 0)
+
+def on_message(mqttc, obj, msg):
+    global count, total_latency
+
+    count += 1
+    total_latency += float(msg.payload.decode("utf-8"))
+
+mqtts = mqtt.Client()
+mqtts.message_callback_add(mqtt_topic, on_message)
+mqtts.on_connect = on_connect
+mqtts.username_pw_set(mqtt_username,mqtt_password)
+
+if args.get('mqtt_tls'):
+    mqtts.tls_set(cert_reqs=ssl.CERT_NONE)
+    mqtts.tls_insecure_set(True)
+
+mqtts.connect(mqtt_address, int(mqtt_port), 60)
+mqtts.loop_start()
+
+mqttp = mqtt.Client()
+mqttp.username_pw_set(mqtt_username,mqtt_password)
+
+if args.get('mqtt_tls'):
+    mqttp.tls_set(cert_reqs=ssl.CERT_NONE)
+    mqttp.tls_insecure_set(True)
+
+mqttp.connect(mqtt_address, int(mqtt_port), 60)
+mqttp.loop_start()
 
 # Read image
 img = load_image(image_file)
@@ -161,9 +221,7 @@ print('Start benchmarking...')
 
 # Starting workers
 for i in range(num_workers):
-    worker = threading.Thread(target=benchmark_worker, args=())
-    worker.setDaemon(True)
-    worker.start()
+    threading.Thread(target=benchmark_worker, args=(), daemon=True).start()
 
 while True:
     count = 0
